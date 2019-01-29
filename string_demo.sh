5cea68dcd58204d07dfb9f5e0f12b81d400b1757
shuoshuren <534361124@qq.com>
2019-01-30 00:11:48 +0800

    字符串操作

---
+++ string_demo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# 字符串操作
+str="helloWorld,好好学习"
+length=`expr length $str `
+echo "字符串长度：$length"
+echo "取子串：`expr substr $str 1 5`"
+echo "取子串：`echo ${str:0:5}`"
+echo "连接：${str},天天向上"
+echo "替换一次：`echo ${str/好/good }`"
+echo "替换全部：`echo ${str//好/good }`"
